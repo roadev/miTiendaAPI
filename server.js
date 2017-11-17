e05777665bda8fc4a7d58ac830ec39dfa5d9fc34
Juan Roa <jdroa92@gmail.com>
2017-11-16 23:15:44 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const Post = require('./api/models/postModel');
+const bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Blogdb');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/postRoutes');
+routes(app);
+
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
+
+app.listen(port);
+
+console.log('post list RESTful API server started on: ' + port);
